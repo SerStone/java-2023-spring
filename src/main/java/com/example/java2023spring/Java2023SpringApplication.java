4fcb0b1be437dfe7fae0931cf2bc565078521147
SerStone <mishagoal752@gmail.com>
2023-10-24 20:50:36 +0300

    add hw1 spring

---
+++ src/main/java/com/example/java2023spring/Java2023SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.java2023spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2023SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java2023SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java2023spring/Java2023SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java2023spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2023SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
